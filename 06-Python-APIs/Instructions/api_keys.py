c22961c3e76da991cd8235f60df237b80ee46c40
Chandler Gibbons <chandlergibbons6@gmail.com>
2020-12-28 22:03:42 -0500

    weather code almost complete requires dates, saved png's, and regression writeup

--- 06-Python-APIs/Instructions/api_keys.py
+++ 06-Python-APIs/Instructions/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "010221efe0fe74c3c38125c2372c4c99"
+weather_api_key = "40c2e59ad1e40b8c9d6c88330ec34103"
 
 # Google API Key
 g_key = "AIzaSyCyRGq43trKs70rmO3tCY72XxVarNCHQfM"
